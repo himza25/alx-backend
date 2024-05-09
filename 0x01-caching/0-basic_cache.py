a340f3e50d5ee137aa5d925a48a865b3263ae943
himza25 <himza.of.theking95@gmail.com>
2024-05-08 17:17:30 -0700

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" BasicCache module
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class that inherits from BaseCaching
+        A basic caching system with no size limit
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
