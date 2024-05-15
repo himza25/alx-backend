cd90ddfe3042cf914fc05c9c239245677a65cf90
himza25 <himza.of.theking95@gmail.com>
2024-05-14 19:59:21 -0700

    update

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,42 +1,25 @@
 #!/usr/bin/env python3
-"""
-Basic Babel setup
-"""
-from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+""" 3-app module """
+from typing import Union
+from flask import Flask, request
+from flask_babel import Babel
+from routes.routes_3 import app_routes
+from config import Config
 
 
 app = Flask(__name__)
 babel = Babel(app)
 
-
-class Config:
-    """
-    config for your Flask app
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
 app.config.from_object(Config)
+app.register_blueprint(app_routes)
 
 
 @babel.localeselector
-def get_locale() -> str:
+def get_locale() -> Union[str, None]:
+    """ get locale
     """
-    Get locale from request
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
-@app.route('/')
-def main() -> str:
-    """
-    simply outputs Welcome to Holberton
-    """
-    return render_template('3-index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
