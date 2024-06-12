095f7babbe955d221e47bb137ce6060f127f28ef
himza25 <himza.of.theking95@gmail.com>
2024-06-12 08:37:49 -0700

    task 1

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,42 @@
+import { createClient, print } from 'redis';
+
+// Create a Redis client
+const client = createClient();
+
+// Event listener for successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Event listener for connection errors
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+/**
+ * Set a new school in Redis
+ * @param {string} schoolName - The key to set in Redis
+ * @param {string} value - The value to set for the key
+ */
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+/**
+ * Display the value of a school from Redis
+ * @param {string} schoolName - The key to retrieve from Redis
+ */
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      console.error(`Error retrieving value for ${schoolName}: ${err.message}`);
+    } else {
+      console.log(reply);
+    }
+  });
+}
+
+// Call the functions with the specified arguments
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
