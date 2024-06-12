cc6dc0c03c4d826d16b4826459d6695fd8549785
himza25 <himza.of.theking95@gmail.com>
2024-06-12 08:55:38 -0700

    task 6

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,28 @@
+import kue from 'kue';
+
+// Create a Kue queue
+const queue = kue.createQueue();
+
+// Define job data
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'This is a test notification'
+};
+
+// Create a job in the queue
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
+
+// Handle job completion
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+// Handle job failure
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
